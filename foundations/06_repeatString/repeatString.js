b4f246007dddc07d7e365cdd15b2eca43ca3cab9
mustafaHasan06 <mustafahasan06.tech@gmail.com>
2026-01-05 12:37:14 +0200

    Finish Ex.7
    easy

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,n) {
+    if(n < 0 ) return "ERROR";
+    let res='';
+    while(n > 0){
+        res+=str;
+        n--;
+    }
+    return res;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+3
