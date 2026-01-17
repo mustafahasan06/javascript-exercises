ec59142131ca194ad06669a991bb89fb46a917aa
mustafaHasan06 <mustafahasan06.tech@gmail.com>
2026-01-17 19:39:22 +0200

    finish the final one ... ex 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {    
+   return people.reduce((oldest,person) => {
+        const oldestAge = getAge(oldest.yearOfBirth , oldest.yearOfDeath);
+        const personAge = getAge(person.yearOfBirth , person.yearOfDeath);
+        return personAge >oldestAge?  person : oldest;
+    })
 };
-
+const getAge = (birth, death) => {
+  if (!death) death = new Date().getFullYear(); 
+  return death - birth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
