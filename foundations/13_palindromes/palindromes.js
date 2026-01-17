d5c39c9331c0f8f65d4bf4b816cff4489ce7e689
mustafaHasan06 <mustafahasan06.tech@gmail.com>
2026-01-17 19:14:06 +0200

    finish ex 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const str = string.toLowerCase().replace(/[^a-z0-9]/g, "");    let l =0;
+    let r= str.length-1;
+    while(l < r){
+        if(str.charAt(l)!= str.charAt(r)) return false;
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
